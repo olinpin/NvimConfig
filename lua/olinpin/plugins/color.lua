ebbe4498d1b2a2f4c60b23646ddfd21d68aae9e0
oliverhnat <oliver.hnat@gmail.com>
2023-08-28 21:32:49 +0200

    Added new theme

--- lua/olinpin/plugins/color.lua
+++ lua/olinpin/plugins/color.lua
@@ -1,17 +1,26 @@
-function ColorMe(color)
-    color = color or "catppuccin"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
+-- function ColorMe(color)
+--     color = color or "catppuccin"
+--     vim.cmd.colorscheme(color)
+--
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+--     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+--
+-- return {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+--     init = function()
+--         ColorMe()
+--         vim.o.background = "dark"
+--     end
+-- }
 
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    init = function()
-        ColorMe()
-        vim.o.background = "dark"
+    'mhartington/oceanic-next',
+    init = function ()
+        -- require('oceanic-next').setup()
+        vim.cmd.colorscheme('OceanicNext')
+        vim.cmd("highlight! EndOfBuffer guibg=background guifg=background")
     end
 }
--- lua/olinpin/plugins/harpoon.lua
+++ lua/olinpin/plugins/harpoon.lua
@@ -39,6 +39,6 @@ return {
         vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
         vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
         vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
-        vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
+        vim.cmd('highlight! TabLineFill guibg=NONE guifg=background')
     end
 }
