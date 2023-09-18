f04c2cb2c9cdcb6599cf3e35d77bd8b585bb1313
oliverhnat <oliver.hnat@gmail.com>
2023-09-18 13:51:12 +0200

    Added another color theme

--- lua/olinpin/plugins/color.lua
+++ lua/olinpin/plugins/color.lua
@@ -1,26 +1,25 @@
--- function ColorMe(color)
---     color = color or "catppuccin"
---     vim.cmd.colorscheme(color)
---
---     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
---     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- end
---
--- return {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     priority = 1000,
---     init = function()
---         ColorMe()
---         vim.o.background = "dark"
---     end
--- }
+function ColorMe(color)
+    color = color or "OceanicNext"
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
 
 return {
-    'mhartington/oceanic-next',
-    init = function ()
-        -- require('oceanic-next').setup()
-        vim.cmd.colorscheme('OceanicNext')
-        vim.cmd("highlight! EndOfBuffer guibg=background guifg=background")
-    end
+    {
+        'mhartington/oceanic-next',
+        config = function ()
+            -- require('oceanic-next').setup()
+            vim.cmd.colorscheme('OceanicNext')
+            vim.cmd("highlight! EndOfBuffer guibg=background guifg=background")
+        end
+    }
+    -- {
+    --     'nxvu699134/vn-night.nvim',
+    --     config = function ()
+    --         vim.cmd.colorscheme('vn-night')
+    --         vim.cmd("highlight! EndOfBuffer guibg=background guifg=background")
+    --     end
+    -- }
 }
--- lua/olinpin/plugins/lualine.lua
+++ lua/olinpin/plugins/lualine.lua
@@ -1,11 +1,11 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-	config = function ()
-		require('lualine').setup({
-			options = {
-				theme = 'ayu_mirage'
-			}
-		})
-	end
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+    config = function ()
+        require('lualine').setup({
+            options = {
+                theme = 'ayu_mirage'
+            }
+        })
+    end
 }
