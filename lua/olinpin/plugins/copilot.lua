7b38c912f7dc60201ad3eecd5e05f3b8876cebb5
oliverhnat <oliver.hnat@gmail.com>
2023-11-18 15:33:39 +0100

    Changed the keybind for copilot

--- lua/olinpin/plugins/copilot.lua
+++ lua/olinpin/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
                 enabled = true,
                 auto_trigger = true,
                 keymap = {
-                    accept = "<C-;>"
+                    accept = "…"
                 }
             },
             panel = {
