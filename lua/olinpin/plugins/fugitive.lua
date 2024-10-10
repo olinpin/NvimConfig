38d76d11534fd43e16b089899ef7b9cfa3c38452
Oliver <oliver@elnino.tech>
2024-10-10 13:23:24 +0200

    feat(fugitive): add <leader>ga to stage current file

--- lua/olinpin/plugins/fugitive.lua
+++ lua/olinpin/plugins/fugitive.lua
@@ -6,5 +6,6 @@ return {
         vim.keymap.set("n", "<leader>gpl", ":G pull<CR>", { desc = "Git pull" })
         vim.keymap.set("n", "<leader>gps", ":G push<CR>", { desc = "Git push" })
         vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
+        vim.keymap.set("n", "<leader>ga", ":Gwrite<CR>", { desc = "Git add current file" })
     end
 }
