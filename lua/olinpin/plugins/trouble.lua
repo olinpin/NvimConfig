3c7bf569f5a5d00f8d801e7676282f4bb5274a90
oliverhnat <oliver.hnat@gmail.com>
2024-11-01 16:19:48 +0100

    feat(trouble): add keybinds to trouble

--- lua/olinpin/plugins/trouble.lua
+++ lua/olinpin/plugins/trouble.lua
@@ -6,5 +6,17 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
     },
-    event = "VeryLazy"
+    event = "VeryLazy",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+    }
 }
